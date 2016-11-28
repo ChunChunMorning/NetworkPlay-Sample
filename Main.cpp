e9e852726f2e3bbf965092111615a0090b428f9f
Asachun <chuchuchu.chunchun@gmail.com>
2016-11-28 11:19:33 +0900

    Add Bar.

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+﻿# include <Siv3D.hpp>
+
+void Main()
+{
+	Rect leftBar(50, 180, 30, 120);
+	Rect rightBar(560, 180, 30, 120);
+
+	while (System::Update())
+	{
+		leftBar.draw();
+		rightBar.draw();
+	}
+}
