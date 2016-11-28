cb5e330106ba21ddd8686a3a3bf3f211fb723cea
Asachun <chuchuchu.chunchun@gmail.com>
2016-11-28 16:03:09 +0900

    Implement SceneManager.

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,72 @@
 ﻿
 # include <Siv3D.hpp>
+# include <HamFramework.hpp>
 
-int GetInput()
+struct GameData
 {
-	int move = 0;
 
-	if (Input::KeyUp.pressed)
-		move -= 1;
+};
 
-	if (Input::KeyDown.pressed)
-		move += 1;
-
-	return move;
-}
-
-void Main()
+class Game : public SceneBase<String, GameData>
 {
-	Rect leftBar(50, 180, 30, 120);
-	Rect rightBar(560, 180, 30, 120);
-	Circle ball(320, 240, 10);
-	Vec2 ballSpeed = Vec2(-1, 1);
+private:
 
-	while (System::Update())
+	Rect leftBar;
+	Rect rightBar;
+	Circle ball;
+	Vec2 ballSpeed;
+
+	int getInput()
+	{
+		int move = 0;
+
+		if (Input::KeyUp.pressed)
+			move -= 1;
+
+		if (Input::KeyDown.pressed)
+			move += 1;
+
+		return move;
+	}
+
+public:
+
+	void init() override
+	{
+		leftBar = Rect(50, 180, 30, 120);
+		rightBar = Rect(560, 180, 30, 120);
+		ball = Circle(320, 240, 10);
+		ballSpeed = Vec2(-1, 1);
+	}
+
+	void update() override
 	{
 		ball.center += 3 * ballSpeed;
-		leftBar.y += 3 * GetInput();
-		rightBar.y += 3 * GetInput();
+		leftBar.y += 3 * getInput();
+		rightBar.y += 3 * getInput();
 
 		if (ball.center.y < ball.r || ball.center.y > Window::Height() - ball.r)
 			ballSpeed.y = -ballSpeed.y;
 
 		if (ball.intersects(leftBar) || ball.intersects(rightBar))
 			ballSpeed.x = -ballSpeed.x;
+	}
 
+	void draw() const override
+	{
 		ball.draw(Palette::Yellow);
 		leftBar.draw();
 		rightBar.draw();
+	}
+};
+
+void Main()
+{
+	SceneManager<String, GameData> manager;
+	manager.add<Game>(L"game");
+
+	while (System::Update())
+	{
+		manager.updateAndDraw();
 	}
 }
